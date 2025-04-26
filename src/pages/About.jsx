fafee89eeea786772b4e7e9be175bccc041860fc
Charlse Mbithi <33979814+Mbicha@users.noreply.github.com>
2025-04-26 19:51:49 +0300

    Update About.jsx
    About us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,10 +7,15 @@ const About = () => {
             <div className="flex flex-col items-center mt-16 w-4/5">
                 <h1 className=" flex justify-center text-3xl font-bold text-white mb-6">About</h1>
                 <p className="flex justify-center text-2xl text-white">
-                    I’m a web developer skilled in the MERN stack and Tailwind CSS,
-                    specializing in building responsive, scalable, and user-friendly
-                    applications. I focus on creating seamless experiences through
-                    clean, efficient code and modern design.
+                    I’m a civil engineer with hands-on experience in infrastructure development, 
+                    specializing in precast concrete construction, urban road construction, and 
+                    rehabilitation. I have worked on major metro rail and municipal projects, 
+                    gaining practical exposure to the manufacturing of precast elements and tunnel 
+                    boring operations while maintaining strict health and safety standards. 
+                    My expertise also includes road concretization and traffic management during 
+                    execution phases. Additionally, I am proficient in AutoCAD, Revit, STAAD.Pro, 
+                    and OfficeSuite, enabling me to deliver detailed structural designs, comprehensive 
+                    analyses, and high-quality project documentation that meet modern construction demands.
                 </p>
             </div>
             
